204d989573c4bbee7f57c9bcdfdcb53049364b8e
Logeln <logelnln@gmail.com>
2026-06-13 23:50:07 +0700

    main->serialize

--- src/serialize.c
+++ src/serialize.c
@@ -1,13 +1,6 @@
-#include <stdio.h>
-#include <string.h>
-#include "defines.h"
-#include "message.h"
-void serialise(struct message m, char *s){
+void serialize(struct message m, char *s){
     if(m.type == SERVER_CLIENT)
         sprintf(s,"ID: %d\nReply-ID: %d\nUsername: %s\nBody: %s", m.id, m.reply_id, m.username, m.body);
     else
         sprintf(s,"Reply-ID: %d\nBody: %s", m.reply_id, m.body);
-}
-int main(){
-	
-}
+z
---
+++ src/serialize.h
@@ -0,0 +1,5 @@
+#include <stdio.h>
+#include <string.h>
+#include "defines.h"
+#include "message.h"
+void serialize(struct message, char *);
