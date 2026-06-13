88e597efa773ec827f86751c3f2cb441c7ed144d
Logeln <logelnln@gmail.com>
2026-06-14 00:10:44 +0700

    sprintf->snprintf

--- src/serialize.c
+++ src/serialize.c
@@ -1,6 +1,6 @@
 void serialize(struct message m, char *s){
     if(m.type == SERVER_CLIENT)
-        sprintf(s,"ID: %d\nReply-ID: %d\nUsername: %s\nBody: %s", m.id, m.reply_id, m.username, m.body);
+        snprintf(s,sizeof(s),"ID: %d\nReply-ID: %d\nUsername: %s\nBody: %s", m.id, m.reply_id, m.username, m.body);
     else
-        sprintf(s,"Reply-ID: %d\nBody: %s", m.reply_id, m.body);
-z
+        snprintf(s,sizeof(s),"Reply-ID: %d\nBody: %s", m.reply_id, m.body);
+}
--- src/serialize.h
+++ src/serialize.h
@@ -1,5 +1,8 @@
+#ifndef SERIALIZE_H_GUARD
+#define SERIALIZE_H_GUARD
 #include <stdio.h>
 #include <string.h>
 #include "defines.h"
 #include "message.h"
 void serialize(struct message, char *);
+#endif /*SERIALIZE_H_GUARD*/
