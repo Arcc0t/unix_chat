da239d2dc19514d74e6a31d7fd187c08a84cbe2b
Logeln <logelnln@gmail.com>
2026-06-14 00:28:20 +0700

    Buffer overflowing

--- src/serialize.c
+++ src/serialize.c
@@ -1,6 +1,8 @@
-void serialize(struct message m, char *s){
-    if(m.type == SERVER_CLIENT)
-        snprintf(s,sizeof(s),"ID: %d\nReply-ID: %d\nUsername: %s\nBody: %s", m.id, m.reply_id, m.username, m.body);
-    else
-        snprintf(s,sizeof(s),"Reply-ID: %d\nBody: %s", m.reply_id, m.body);
+BOOL serialize(struct message *m, char *s){
+	int n;
+	if(m.type == SERVER_CLIENT)
+		n = snprintf(s,sizeof(s),"ID: %d\nReply-ID: %d\nUsername: %s\nBody:\n%s", m->id, m->reply_id, m->username, m->body);
+	else
+		n = snprintf(s,sizeof(s),"Reply-ID: %d\nBody:\n%s", m->reply_id, m->body);
+	return n < sizeof(s) || s[n-1]=='\0';
 }
