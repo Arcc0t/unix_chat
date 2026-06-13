5f2a400fc85a0e88eb5cbcf7f1ebb2216710af8c
Arcc0t <kirlamch2008@gmail.com>
2026-06-13 19:11:52 +0300

    message structure added

---
+++ src/defines.h
@@ -0,0 +1,23 @@
+#ifndef DEFINES_H_GUARD
+#define DEFINES_H_GUARD
+
+#define TO_BOOL(x) (!!(x))
+
+#ifndef NULL
+#define NULL ((void *) 0) 
+#endif /* NULL */
+
+typedef long long		Long;
+typedef unsigned short		ushort;
+typedef unsigned int		uint;
+typedef unsigned long		ulong;
+typedef unsigned long long	uLong;
+typedef unsigned char		byte;
+typedef signed char		sbyte;	
+typedef char 			BOOL;
+
+
+enum {FALSE = 0, TRUE = 1};
+
+
+#endif /* DEFINES_H_GUARD */
---
+++ src/message.h
@@ -0,0 +1,30 @@
+#ifndef MESSAGE_H_GUARD
+#define MESSAGE_H_GUARD
+
+enum {USERNAME_MAXLEN = 32, BODY_MAXLEN = 4096};
+
+enum field_type {
+	FIELD_ID = 0,
+	FIELD_REPLY_ID,
+	FIELD_USERNAME,
+	FIELD_BODY,
+	FIELD_COUNT /* number of fields overall */
+};
+
+enum message_type {CLIENT_SERVER, SERVER_CLIENT};
+
+struct message {
+	enum message_type type;
+	int id;
+	int reply_id;
+	char username[USERNAME_MAXLEN];
+	char body[BODY_MAXLEN];
+};
+
+enum {MESSAGE_LEN = sizeof(struct message)}; /* might be more than 
+						needed to store 
+						a message due to
+						alignment */
+
+
+#endif /*MESSAGE_H_GUARD*/
