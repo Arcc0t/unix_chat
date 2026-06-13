2e51e8cc8097a5e59dba5e4c4be10ff9fae42a60
logeln <trueoldd@gmail.com>
2026-06-14 00:38:59 +0700

    Update serialize.c
    sizeof->N

--- src/serialize.c
+++ src/serialize.c
@@ -1,8 +1,8 @@
-BOOL serialize(struct message *m, char *s){
+BOOL serialize(struct message *m, char *s, int N){
 	int n;
 	if(m.type == SERVER_CLIENT)
-		n = snprintf(s,sizeof(s),"ID: %d\nReply-ID: %d\nUsername: %s\nBody:\n%s", m->id, m->reply_id, m->username, m->body);
+		n = snprintf(s, N, "ID: %d\nReply-ID: %d\nUsername: %s\nBody:\n%s", m->id, m->reply_id, m->username, m->body);
 	else
-		n = snprintf(s,sizeof(s),"Reply-ID: %d\nBody:\n%s", m->reply_id, m->body);
-	return n < sizeof(s) || s[n-1]=='\0';
+		n = snprintf(s, N, "Reply-ID: %d\nBody:\n%s", m->reply_id, m->body);
+	return n < N || s[n-1] == '\0';
 }
