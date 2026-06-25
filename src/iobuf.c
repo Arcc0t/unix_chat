ffd4371bc88723b0903380d4b58b67b6905cdedd
Logeln <logelnln@gmail.com>
2026-06-25 16:19:11 +0700

    Add iobuf funcs

--- src/iobuf.c
+++ src/iobuf.c
@@ -94,3 +94,78 @@ size_t iobuf_read(IOBUF *ibuf, void *obuf, size_t obuf_size)
 	}
 	return bytes_read;
 }
+
+size_t iobuf_write(IOBUF *obuf, void *ibuf, size_t ibuf_size){
+	size_t to_write = MIN(ibuf_size, IOBUF_HALF_SIZE - obuf->back_size);
+	memcpy(ibuf, obuf->back, to_write);
+	obuf->back_size+=to_write;
+	return to_write;
+}
+
+int iobuf_getchar(IOBUF *buf){
+	if(buf->front_size)
+		return buf->front[--buf->front_size];
+	else
+		return IOBUF_EOF;
+}
+
+int iobuf_peek(IOBUF *buf){
+	if(buf->front_size)
+		return buf->front[buf->front_size - 1];
+	else
+		return IOBUF_EOF;
+}
+
+BOOL iobuf_putchar(IOBUF *buf, char c){
+	if(buf->back_size == IOBUF_HALF_SIZE)
+		return FALSE;
+	else{
+		buf->back[buf->backsize++] = c;
+		return TRUE;
+	}
+}
+
+size_t iobuf_getline(IOBUF *ibuf, char *obuf, size_t obuf_size){
+	char c;
+	size_t len = 0;
+	while(obuf_size && ibuf->front_size){
+		c = iobuf_getchar(ibuf);
+		if(c){
+			*(obuf++) = c;
+			obuf_size--;
+			len++;
+			if(c == '\n')break;
+		}
+		else break;
+	}
+	return len;
+}
+
+iobuf_res iobuf_putline(IOBUF *buf, char *str){
+	char  c;
+	while(TRUE){
+		c=*(str++);
+		if(c)
+			if(!iobuf_putchar(buf, c))return IOBUF_FULL;
+			else if(c== '\n')return IOBUF_PARTIAL;
+		else
+			return IOBUF_PARTIAL;
+	}
+}
+
+size_t iobuf_fwrite(IOBUF *buf, int fd){
+	if(!buf->front_size)return 0;	
+	char tmp[buf->front_size];
+	for(char *i = tmp, *j = buf->front + buf->front_size - 1; j >= buf->front; i++, j--)
+		*i=*j;
+	size_t len = write(fd, tmp, buf->front_size);
+	buf->front_size-=len;
+	return len;
+}
+
+iobuf_res iobuf_flush(IOBUF *buf){
+	while(buf->back_size && buf->front_size < IOBUF_HALF_SIZE)
+		buf->front[buf->front_size++] = buf->back[--buf->back_size];
+	if(buf->back_size)return IOBUF_FULL;
+	return IOBUF_PARTIAL;
+}
--- src/iobuf.h
+++ src/iobuf.h
@@ -9,33 +9,33 @@ typedef struct iobuf_ IOBUF;
 enum {IOBUF_NOFILE = -1};
 typedef enum {IOBUF_ERR = -1, IOBUF_FULL = 0, IOBUF_PARTIAL = 1, IOBUF_EOF} iobuf_res;
 
-IOBUF *iobuf_create(int infd, int outfd);
+IOBUF *iobuf_create(int infd, int outfd);//+
 
-void iobuf_destroy(IOBUF *buf);
+void iobuf_destroy(IOBUF *buf);//+
 
 
 /* write to buffer */
-size_t iobuf_write(IOBUF *obuf, void *ibuf, size_t ibuf_size);
+size_t iobuf_write(IOBUF *obuf, void *ibuf, size_t ibuf_size);//+
 
 /* read from buffer */
-size_t iobuf_read(IOBUF *ibuf, void *obuf, size_t obuf_size);
+size_t iobuf_read(IOBUF *ibuf, void *obuf, size_t obuf_size);//+
 
-int iobuf_getchar(IOBUF *buf);
+int iobuf_getchar(IOBUF *buf);//+
 
-int iobuf_peek(IOBUF *buf);
+int iobuf_peek(IOBUF *buf);//+
 
-BOOL iobuf_putchar(IOBUF *buf, char c);
+BOOL iobuf_putchar(IOBUF *buf, char c);//+
 
-size_t iobuf_getline(IOBUF *ibuf, char *obuf, size_t obuf_size);
+size_t iobuf_getline(IOBUF *ibuf, char *obuf, size_t obuf_size);//+
 
-iobuf_res iobuf_putline(IOBUF *buf, char *str);
+iobuf_res iobuf_putline(IOBUF *buf, char *str);//+
 
 /* write to file descriptor*/
-size_t iobuf_fwrite(IOBUF *buf, int fd); 
+size_t iobuf_fwrite(IOBUF *buf, int fd); //+
 
 /*read from file descriptor*/
-ssize_t iobuf_fread(IOBUF *buf, int fd);
+ssize_t iobuf_fread(IOBUF *buf, int fd);//+
 
-iobuf_res iobuf_flush(IOBUF *buf);
+iobuf_res iobuf_flush(IOBUF *buf);//+
 
 #endif /* IOBUF_H_GUARD */
